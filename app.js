fb151864b95131ff6bff1899a429829364747f5c
Mark Prangan <pranganmark@gmail.com>
2023-08-31 14:49:27 +1000

    Set up game board on screen

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ const player1 = player('Mark', 'X');
 const player2 = player('John', 'O');
 
 console.log(player1.name)
+
+const gameBoard = (() => {
+    const boardState =
+        [
+            ["X", "O", "X"],
+            ["O", "X", "O"],
+            ["X", "O", "X"]
+        ]
+    return { boardState }
+})();
