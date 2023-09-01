c8fe2bce75973f7a7b108fc3f5286050035938e4
Mark Prangan <pranganmark@gmail.com>
2023-09-01 11:56:27 +1000

    Created function that checks for 3 in a row or draw and displays result on screen

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const board = document.querySelector(".board");
-var turn = 0;
+const container = document.querySelector(".container")
 
 const player = (name, marker) => {
     return { name, marker };
@@ -9,11 +9,13 @@ const player1 = player('Player 1', 'X');
 const player2 = player('Player 2', 'O');
 
 const gameBoard = (() => {
+    var turn = 0;
+    var winner = false;
     const boardState =
         [
-            ["-", "-", "-"],
-            ["-", "-", "-"],
-            ["-", "-", "-"]
+            ["1", "2", "3"],
+            ["4", "5", "6"],
+            ["7", "8", "9"]
         ]
 
     const createBoard = () => {
@@ -32,18 +34,55 @@ const gameBoard = (() => {
             }
         }
     }
+
+
+
     const addMarker = (place) => {
         turn += 1
+        var player;
         if (turn % 2 != 0) {
-            place.innerText = player1.marker;
+            player = player1.name
+            marking = player1.marker;
+            place.innerText = marking;
         } else {
+            player = player2.name
             place.innerText = player2.marker;
         }
         var i = place.dataset.rowIndex;
         var j = place.dataset.colIndex;
         boardState[i][j] = place.innerText;
+        checkBoard(boardState, player);
     }
-    return { boardState, createBoard, addMarker }
+
+    const displayResults = (player, winner) => {
+        const results = document.createElement("span");
+        results.classList = 'results';
+        if (winner == true) {
+            results.innerText = `${player} Wins!`;
+            container.appendChild(results);
+        } else {
+            results.innerText = `Draw!`;
+            container.appendChild(results);
+        }
+    }
+
+    const checkBoard = (boardState, player) => {
+        if (boardState[0][0] == boardState[0][1] && boardState[0][1] == boardState[0][2]
+            || boardState[1][0] == boardState[1][1] && boardState[1][1] == boardState[1][2]
+            || boardState[2][0] == boardState[2][1] && boardState[2][1] == boardState[2][2]
+            || boardState[0][0] == boardState[1][0] && boardState[1][0] == boardState[2][0]
+            || boardState[0][1] == boardState[1][1] && boardState[1][1] == boardState[2][1]
+            || boardState[0][2] == boardState[1][2] && boardState[1][2] == boardState[2][2]
+            || boardState[0][0] == boardState[1][1] && boardState[1][1] == boardState[2][2]
+            || boardState[0][2] == boardState[1][1] && boardState[1][1] == boardState[2][0]) {
+            winner = true;
+            displayResults(player, winner);
+        } else if (turn == 9) {
+            displayResults(winner)
+        }
+    }
+
+    return { createBoard }
 })();
 
 gameBoard.createBoard()
